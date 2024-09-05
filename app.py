5a2070e9e0525184e9ef555f0d5c199430ff01aa
Emanuel <emano4775@gmail.com>
2024-09-05 20:53:42 -0300

    Adicionando tela de inicio e linkado com medicamento

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,send_from_directory
 
 app = Flask(__name__)
 
 # Tela principal
-@app.route('/')
+@app.route('/medicamentos')
 def index():
     medicamentos = [
         {'nome': 'Medicamento A', 'preco': 'R$ 49,99', 'imagem': '/static/img/medicamentoA.png'},
         {'nome': 'Medicamento B', 'preco': 'R$ 29,99', 'imagem': '/static/img/medicamentoB.png'}
     ]
     return render_template('index.html', medicamentos=medicamentos)
+
+@app.route('/videos/<path:filename>')
+def download_file(filename):
+    return send_from_directory('static/videos', filename)
+
+# Rota para a página de início
+@app.route('/')
+def inicio():
+    return render_template('inicio.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
